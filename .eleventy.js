85bbc9ba67dec20135455fe8538aecb821f621bc
Alan Moore <contact@alanmoore.info>
2021-02-24 11:17:55 -0500

    Added static and image passthru to 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
-module.exports = {
-    dir: {
-        // includes: "_includes",
-        layouts: "_layouts",
-        data: "_data"
-    }
+module.exports = function(eleventyConfig) {
+    // Output directory: _site
+
+    // Copy `static/` to `_site/static`
+    // If you use a subdirectory, it’ll copy using the same directory structure.
+    eleventyConfig.addPassthroughCopy("static");
+    eleventyConfig.addPassthroughCopy("images");
+
+
+    return {
+        dir: {
+            // includes: "_includes",
+            layouts: "_layouts",
+            data: "_data",
+        }
+    };
 };
